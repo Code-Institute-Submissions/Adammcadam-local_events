9fb19a84a2c955fcc99bbdf3fabb5a5dbb206b04
Ubuntu <ubuntu@ip-172-31-29-140.ec2.internal>
2019-10-28 18:35:30 +0000

    added templates and get bands method

--- app.py
+++ app.py
@@ -1,16 +1,29 @@
 import os
-from flask import Flask
+from flask import Flask, render_template, redirect, request, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
 
 app = Flask(__name__)
 
+app.config["MONGO_DBNAME"] = 'local_events'
+app.config["MONGO_URI"] = 'mongodb+srv://root:eu4QBwmtZMzd0HU6@myfirstcluster-xm8rq.mongodb.net/local_events?retryWrites=true&w=majority'
+# app.config["MONGO_URI"] = os.environ.get('MONGO_URI')
+
+mongo = PyMongo(app)
+
 @app.route('/')
-def hello():
-    return 'sup, bitches!'
+@app.route('/get_bands')
+def get_bands():
+    return render_template("bands.html", bands=mongo.db.bands.find())
+
     
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
-    port=int(os.environ.get('PORT')),
-    debug=True)
+            port=int(os.environ.get('PORT')),
+            debug=True)
+            
+
+            
     
     
     
