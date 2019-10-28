49fa6cc5c551854c8bd9ae91e0dd6f3955455a62
Ubuntu <ubuntu@ip-172-31-29-140.ec2.internal>
2019-10-28 17:44:10 +0000

    initial commit

--- app.py
+++ app.py
@@ -1,20 +1,16 @@
 import os
-from flask import Flask, render_template, redirect, request, url_for
-from flask_pymongo import PyMongo
+from flask import Flask
 
 app = Flask(__name__)
 
-app.config['MONGO_URI'] = os.environ.get("MONGO_URI")
-app.config.from_object(Config)s
-
-mongo = PyMongo(app)
-
 @app.route('/')
-@app.route('/index')
-def index():
-    return render_template('index.html', title='Home')
-
+def hello():
+    return 'sup, bitches!'
+    
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
-        port=int(os.environ.get('PORT')),
-        debug=True)
+    port=int(os.environ.get('PORT')),
+    debug=True)
+    
+    
+    
